55514cf0890f6375dfffe13e942216b0aa4af593
sheth chirag <102785098+shethchirag@users.noreply.github.com>
2024-09-20 21:46:56 +0530

    setup controller

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
-import express from "express";
 import dbConnect from "./db/dbConnect.js";
 import * as dotenv from "dotenv";
-import app from "./app.js";
+import { app } from "./app.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 dbConnect()
   .then(() => {
-    app.on("error", (err) => {
-      console.log(err);
-    });
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
@@ -20,19 +14,3 @@ dbConnect()
   .catch((err) => {
     console.log(err);
   });
-// const app = express();
-
-// (async function () {
-//   try {
-//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//     app.on("error", (err) => {
-//       console.log(err);
-//       throw err;
-//     });
-//     app.listen(`${process.env.PORT}`, () => {
-//       console.log(`app is listening on ${process.env.PORT}`);
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// })();
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,19 +1,5 @@
-// const asyncHandler = (FN) => async (req, res, next) => {
-//   try {
-//     await FN(req, res, next);
-//   } catch (error) {
-//     res.status(error.code || 500).json({
-//       success: false,
-//       message: error.message,
-//     });
-//   }
-// };
-
-const asyncHandler = (FN) => {
-  (req, res, next) => {
-    Promise.resolve(FN(req, res, next)).catch((err) => {
-      next(err);
-    });
+export const asyncHandler = (FN) => {
+  return (req, res, next) => {
+    Promise.resolve(FN(req, res, next)).catch((err) => next(err));
   };
 };
-export { asyncHandler };
